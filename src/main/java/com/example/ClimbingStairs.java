93793d62f02bfbd096e595fc909dadc93e32f8f3
Gabriel Diegel <gabriel.diegel@icloud.com>
2022-12-09 10:07:43 -0500

    Add implementation

--- src/main/java/com/example/ClimbingStairs.java
+++ src/main/java/com/example/ClimbingStairs.java
@@ -1,10 +1,41 @@
 package com.example;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ClimbingStairs {
 
-  public static int climbStairs(int n) {
+  private static final Map<Integer, Integer> MEMO = new LinkedHashMap<>();
 
-    return 0;
+  /**
+   * Go from index 0 (still on ground) to index n
+   * <p>
+   *                _4__
+   *             _3_|  |
+   *          _2_|     |
+   *      _1_|         |
+   * __0__|            |
+   *
+   * <p>
+   * @param n number of steps
+   * @return possible ways to get to the last step (index n)
+   */
+  public static int climbStairs(int n) {
+    if (MEMO.containsKey(n)) {
+      return MEMO.get(n);
+    }
+
+    if (n == 1) {
+      return 1;
+    }
+
+    if (n == 2) {
+      return 2;
+    }
+
+    MEMO.put(n, climbStairs(n - 1) + climbStairs(n - 2));
+
+    return MEMO.get(n);
   }
 }
 
--- src/test/java/com/example/ClimbingStairsTest.java
+++ src/test/java/com/example/ClimbingStairsTest.java
@@ -16,4 +16,22 @@ class ClimbingStairsTest {
   void shouldReturnTwoForExample1() {
     Assertions.assertThat(ClimbingStairs.climbStairs(2)).isEqualTo(2);
   }
+
+  /**
+   * Input: n = 3
+   * Output: 3
+   * Explanation: There are three ways to climb to the top.
+   * 1. 1 step + 1 step + 1 step
+   * 2. 1 step + 2 steps
+   * 3. 2 steps + 1 step
+   */
+  @Test
+  void shouldReturnThreeForExample2() {
+    Assertions.assertThat(ClimbingStairs.climbStairs(3)).isEqualTo(3);
+  }
+
+  @Test
+  void shouldReturnThreeForExample3() {
+    Assertions.assertThat(ClimbingStairs.climbStairs(4)).isEqualTo(5);
+  }
 }
